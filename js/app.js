42c9ec1fa9e354d6fef99b6f352bcc6fdd1a3948
Cem Bilge <cbilge@gmail.com>
2016-04-11 19:34:33 +0300

    First push

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var app = angular.module('mainApp', ['ngRoute', 'ngResource']);
+app.config(['$routeProvider', function ($routeProvider) { 
+  $routeProvider 
+    .when('/', { 
+      controller: 'mainController', 
+      templateUrl: '/views/home.html' 
+    }) 
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+}]);
+
+app.controller('mainController', ['$scope', 'Report', function ($scope, Report) {
+    $scope.rep = Report.get();
+
+    $scope.labelClass = function (src) {
+        if (src == "Bloomberg") {
+            return "bbg";
+        }
+        else if (src == "Reuters") {
+            return "rtrs";
+        }
+        else if (src == "FT") {
+            return "ft";
+        }
+        else if (src == "WSJ") {
+            return "wsj";
+        }
+        else if (src == "subheader") {
+            return "sublabel";
+        }
+        else {
+            return "";
+        }
+    };
+
+    $scope.subCheck = function (src) {
+        if (src == "subheader") {
+            return "subtext";
+        }
+        else {
+            return "";
+        }
+    };
+
+    $scope.subSpan = function (src) {
+        if (src == "subheader") {
+            return "glyphicon glyphicon-chevron-right subspan";
+        }
+        else {
+            return "";
+        }
+    };
+
+} ]);
+
+app.factory('Report', ['$resource', function ($resource) {
+    return $resource('/js/data/globalreport.txt', {});
+} ]);
---
+++ js/data/data.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $data = $_POST["data"];
+    $ver = $_POST["ver"];
+    if ($ver == "cbilge") {
+        $myfile = fopen(getenv('OPENSHIFT_REPO_DIR')."js/data/globalreport.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, $data);
+        fclose($myfile);
+        echo "done";
+    }
+}
+elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+    echo "yo";
+}
+?>
+
